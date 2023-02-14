2b888aa1868f4a4af7233ad15d2a33a3a7631a03
sergioj <sergio.juagag@educa.jcyl.es>
2023-02-14 10:08:08 +0000

    Footer / Header (Sin probar)

---
+++ vista/layoutFooter.php
@@ -0,0 +1,7 @@
+<footer>
+    <ul class="col-6 d-flex align-items-center justify-content-center">
+        <li class="ms-3">
+            <p> Footer de prueba </p>
+        </li>
+    </ul>
+</footer>
---
+++ vista/layoutHeader.php
@@ -0,0 +1,30 @@
+<header class="bg-light sticky-top">
+    <div class="row">
+
+        <div class="col-6 col-lg-6 order-lg-1 col-md-4 order-md-1 col-sm-6 order-sm-1 col-xs-6 order-xs-1 d-flex align-items-start">
+            <a href="./"><img src="images/logo.png" /></a>
+        </div>
+
+        <div class="col-6 col-lg-6 order-lg-2 col-md-4 order-md-2 col-sm-12 order-sm-3 col-xs-12 order-xs-3 d-flex align-items-end">
+            <i class="ps-2 bi-search" style="font-size: 2rem"></i>
+        </div>
+
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom border-dark">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="">Arduino 1 </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Arduino 2 </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Arduino 3 </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Arduino 4 </a>
+            </li>
+        </ul>
+    </nav>
+</header>
