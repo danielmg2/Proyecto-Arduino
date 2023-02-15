ba5597e4ddbcbfe52ff27ee3c96fc785b4a07574
Daniel <daniel.mergar.1@educa.jcyl.es>
2023-02-15 12:30:43 +0000

    sql, home y login

--- domotica/vista/layout.php
+++ domotica/vista/layout.php
@@ -10,18 +10,30 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<?php include("./vista/layoutHeader.php"); ?>
+<?php 
+    try{
+        $conexion = new PDO('mysql:host='. HOST. ';dbname=' . BBDD, USER, PASS);
+    }catch(PDOException $e){
+        require './ejecuatarScript.php';
+    }?>
 
 
     <header>
+        <a href="./index.php"><img src="./webroot/img/logo_claudio.png" alt="logo"></a>
+        <h1>Domótica en el Claudio</h1>
+
         <?
+   
         if(estaValidado()){
             ?>
-            <form action="./index.php" method="POST"> 
-                <input type="submit" name="logout" class="btn btn-outline-primary me-2" value="Logout">
+            <form action="./index.php" method="POST" id="logout"> 
+                <input  type="submit" name="logout" class="btn btn-outline-primary me-2" value="Cerrar Sesión">
             </form>
             <?
         }
@@ -32,6 +44,6 @@
         require_once $_SESSION['vista'];
         ?>
     </main>
-    <?php include("./vista/layoutFooter.php") ?>
+    
 </body>
 </html>
