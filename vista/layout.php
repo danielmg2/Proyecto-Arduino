b31ad9a114fa7bc49dd7c80d5ea1203ff827b247
lore1 <lorena.araroj@educa.jcyl.es>
2023-02-15 08:11:41 +0000

    cambios

--- vista/layout.php
+++ vista/layout.php
@@ -6,9 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Domotica en el Claudio</title>
     <link rel="stylesheet" href="./webroot/css/estilos.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 <body>
+
+<?php include("./vista/layoutHeader.php"); ?>
+
+
     <header>
         <?
         if(estaValidado()){
@@ -25,5 +32,6 @@
         require_once $_SESSION['vista'];
         ?>
     </main>
+    <?php include("./vista/layoutFooter.php") ?>
 </body>
 </html>
