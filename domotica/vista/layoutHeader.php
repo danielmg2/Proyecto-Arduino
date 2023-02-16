60ec27381e8d8829920241ae2a2fc8405eec6042
sergioj <sergio.juagag@educa.jcyl.es>
2023-02-16 11:19:03 +0000

    a

--- domotica/vista/layoutHeader.php
+++ domotica/vista/layoutHeader.php
@@ -1,18 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom border-dark">
+    <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="">Arduino 1 </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Arduino 2 </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Arduino 3 </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Arduino 4 </a>
+        </li>
+    </ul>
+</nav>
 
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom border-dark">
-        <ul class="navbar-nav mx-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="">Arduino 1 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Arduino 2 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Arduino 3 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Arduino 4 </a>
-            </li>
-        </ul>
-    </nav>
+</header>
