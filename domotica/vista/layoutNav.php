6dbdce95525459bc5037d0e3eec4306700fc6b19
sergioj <sergio.juagag@educa.jcyl.es>
2023-02-16 12:30:14 +0000

    a

---
+++ domotica/vista/layoutNav.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom border-dark">
+    <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino1" class="btn me-2">
+                    <p> Arduino 1 </p>
+                </button>
+            </form>
+        </li>
+        <li class="nav-item">
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino2" class="btn me-2">
+                    <p> Arduino 2 </p>
+                </button>
+            </form>
+        </li>
+        <li class="nav-item">
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino3" class="btn me-2">
+                    <p> Arduino 3 </p>
+                </button>
+            </form>
+        </li>
+        <li class="nav-item">
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino4" class="btn me-2">
+                    <p> Arduino 4 </p>
+                </button>
+            </form>
+        </li>
+    </ul>
+</nav>
