86fc1583e9818eea91a66a8627a4ae024dff4c1a
sergioj <sergio.juagag@educa.jcyl.es>
2023-02-16 11:34:51 +0000

    a

--- domotica/vista/layoutHeader.php
+++ domotica/vista/layoutHeader.php
@@ -1,16 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom border-dark">
     <ul class="navbar-nav mx-auto">
         <li class="nav-item">
-            <a class="nav-link" href="">Arduino 1 </a>
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino1" class="btn me-2">
+                    <p> Arduino 1 </p>
+                </button>
+            </form>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Arduino 2 </a>
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino2" class="btn me-2">
+                    <p> Arduino 2 </p>
+                </button>
+            </form>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Arduino 3 </a>
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino3" class="btn me-2">
+                    <p> Arduino 3 </p>
+                </button>
+            </form>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Arduino 4 </a>
+            <form action="./index.php" method="post">
+                <button type="submit" name="arduino4" class="btn me-2">
+                    <p> Arduino 4 </p>
+                </button>
+            </form>
         </li>
     </ul>
 </nav>
