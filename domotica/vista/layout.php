ed2b7c2e961185c89db3940f3eec67772f55c72f
Sergio-JG <sergiojbusiness@hotmail.com>
2023-02-17 12:24:25 +0000

    HOME GRAFICOS

--- domotica/vista/layout.php
+++ domotica/vista/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +9,10 @@
     <title>Domotica en el Claudio</title>
     <link rel="stylesheet" href="./webroot/css/estilos.css">
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> e24add1 (HOME GRAFICOS)
     <!-- BOOTSTRAP -->
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -30,11 +35,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
+
 <body>
 
     <?php
     try {
         if (compruebaBase() != "domotica") {
+
             crearBase();
         }
     } catch (PDOException $e) {
@@ -45,9 +52,14 @@
         <a href="./index.php"><img src="./webroot/img/logo.png" alt="logo"></a>
         <!--<h1><span class="letra"> D</span>omótica  <span class="letra">C</span>laudio</h1>-->
         <?
+
         if (estaValidado()) {
         ?>
+<<<<<<< HEAD
             <form action="./index.php" method="POST" id="log">
+=======
+            <form action="./index.php" method="POST">
+>>>>>>> e24add1 (HOME GRAFICOS)
                 <input type="submit" name="logout" id="logout" value="">
             </form>
         <?
@@ -95,6 +107,10 @@
 
 </body>
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> e24add1 (HOME GRAFICOS)
 <!-- CHARTS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
 <script src="./js/app.js"></script>
