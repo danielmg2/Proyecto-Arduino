f43dbcb6c9aa0af0bf7747e31d905eb4b8b209b0
Sergio-JG <sergiojbusiness@hotmail.com>
2023-02-17 12:24:25 +0000

    HOME GRAFICOS

--- domotica/vista/layout.php
+++ domotica/vista/layout.php
@@ -1,33 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Domotica en el Claudio</title>
     <link rel="stylesheet" href="./webroot/css/estilos.css">
+
+    <!-- BOOTSTRAP -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <!-- JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <!-- FONTS -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
 
-<?php 
-    try{
-        
-        if(compruebaBase()!="domotica"){
+    <?php
+    try {
+
+        if (compruebaBase() != "domotica") {
             crearBase();
         }
-    }catch(PDOException $e){
-        
-    }?>
+    } catch (PDOException $e) {
+    } ?>
 
 
     <header>
@@ -36,50 +45,45 @@
         <!--<h1><span class="letra"> D</span>omótica  <span class="letra">C</span>laudio</h1>-->
 
         <?
-   
-        if(estaValidado()){
-            ?>
-            <form action="./index.php" method="POST" > 
-                <input  type="submit" name="logout" id="logout" value="" >
+
+        if (estaValidado()) {
+        ?>
+            <form action="./index.php" method="POST">
+                <input type="submit" name="logout" id="logout" value="">
             </form>
-            <?
+        <?
         }
-?>
+        ?>
     </header>
     <div class="container-fluid">
 
-<nav class="navbar navbar-expand-lg navbar-light   border-dark">
-<ul class="navbar-nav mx-auto">
-    <li class="nav-item">
-        <form action="./index.php" method="post">
-            <button type="submit" name="arduino1" class="btn me-2">
-                <p> Arduino 1 </p>
-            </button>
-    </li>
-    <li class="nav-item">
-            <button type="submit" name="arduino2" class="btn me-2">
-                <p> Arduino 2 </p>
-            </button>
-    </li>
-    <li class="nav-item">
-            <button type="submit" name="arduino3" class="btn me-2">
-                <p> Arduino 3 </p>
-            </button>
-    </li>
-    <li class="nav-item">
-            <button type="submit" name="arduino4" class="btn me-2">
-                <p> Arduino 4 </p>
-            </button>
-        </form>
-    </li>
-</ul>
-</nav>
-</div>
-
-
-
-
-
+        <nav class="navbar navbar-expand-lg navbar-light   border-dark">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <form action="./index.php" method="post">
+                        <button type="submit" name="arduino1" class="btn me-2">
+                            <p> Arduino 1 </p>
+                        </button>
+                </li>
+                <li class="nav-item">
+                    <button type="submit" name="arduino2" class="btn me-2">
+                        <p> Arduino 2 </p>
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button type="submit" name="arduino3" class="btn me-2">
+                        <p> Arduino 3 </p>
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button type="submit" name="arduino4" class="btn me-2">
+                        <p> Arduino 4 </p>
+                    </button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    </div>
 
     <main>
 
@@ -87,6 +91,24 @@
         require_once $_SESSION['vista'];
         ?>
     </main>
-    
+
+    <footer class="bg-light text-center">
+
+        <div class="row">
+
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                © Copyright:
+                <a class="text-dark" href="https://www.claudiomoyano.es/">claudiomoyano.es</a>
+            </div>
+
+        </div>
+
+    </footer>
+
 </body>
+
+<!-- CHARTS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
+<script src="./js/app.js"></script>
+
 </html>
