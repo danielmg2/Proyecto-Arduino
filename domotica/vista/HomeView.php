fd309dece6b987e87206284e9916548b4e4853e4
Daniel <daniel.mergar.1@educa.jcyl.es>
2023-02-28 12:14:50 +0000

    JKLKJLÑ

--- domotica/vista/HomeView.php
+++ domotica/vista/HomeView.php
@@ -1,10 +1,10 @@
 <div class="container-fluid">
 
     <div class="d-flex align-items-center justify-content-center">
-        <button type="button" class="active mx-5" data-bs-target="#carouselGraficos" data-bs-slide-to="0" aria-current="true" aria-label="Slide 1"> Anual </button>
-        <button type="button" class="my-3 mx-5" data-bs-target="#carouselGraficos" data-bs-slide-to="1" aria-label="Slide 2"> Mensual </button>
-        <button type="button" class="my-3 mx-5" data-bs-target="#carouselGraficos" data-bs-slide-to="2" aria-label="Slide 3"> Semanal</button>
-        <button type="button" class="my-3 mx-5" data-bs-target="#carouselGraficos" data-bs-slide-to="3" aria-label="Slide 4"> Diario </button>
+        <button type="button" class="active mx-5 boton" data-bs-target="#carouselGraficos" data-bs-slide-to="0" aria-current="true" aria-label="Slide 1" style="color: #AC6238;font-family: 'Rubik', sans-serif;font-weight: 500;"> Anual </button>
+        <button type="button" class="my-3 mx-5 boton" data-bs-target="#carouselGraficos" data-bs-slide-to="1" aria-label="Slide 2" style="color: #AC6238;font-family: 'Rubik', sans-serif;font-weight: 500;"> Mensual </button>
+        <button type="button" class="my-3 mx-5 boton" data-bs-target="#carouselGraficos" data-bs-slide-to="2" aria-label="Slide 3" style="color: #AC6238;font-family: 'Rubik', sans-serif;font-weight: 500;"> Semanal</button>
+        <button type="button" class="my-3 mx-5 boton" data-bs-target="#carouselGraficos" data-bs-slide-to="3" aria-label="Slide 4" style="color: #AC6238;font-family: 'Rubik', sans-serif;font-weight: 500;"> Diario </button>
     </div>
 
     <div id="carouselGraficos" class="carousel carousel-dark slide" data-bs-ride="carousel">
@@ -39,7 +39,7 @@
                     <h2 class="card-title m-3" style="color: #AC6238;font-family: 'Rubik', sans-serif;font-weight: 500;"> Diario </h2>
                     <canvas class="container h-75 w-75 " id="chart4"> </canvas>
 
-<div >
+<!-- <div >
 
     <div id="carouselGraficos" class="carousel carousel-dark slide" data-bs-ride="carousel">
     
@@ -95,4 +95,4 @@
             <button type="button" class="mx-5" data-bs-target="#carouselGraficos" data-bs-slide-to="3" aria-label="Slide 4"><i class="bi bi-brightness-high"></i> Luminosidad </button>
         </div>
     </div>
-</div>
+</div> -->
